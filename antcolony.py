cf76c4aa47d7c5688f477919c3fde0eb610136f0
SaintBallista <jmaniquehenz@gmail.com>
2018-10-03 09:45:52 -0300

    some code boi

---
+++ antcolony.py
@@ -0,0 +1,39 @@
+
+
+
+def generateSolutions():
+pass
+#TODO
+
+def daemonActions():
+pass
+#TODO
+
+def pheromoneUpdate():
+pass
+#TODO
+
+
+def needTermination():
+pass
+#TODO
+
+def needTermination():
+pass
+#TODO
+
+
+
+
+
+def antcolonyMetaheuristic(data):
+	while !needTermination():
+		generateSolutions()
+		daemonActions()
+		pheromoneUpdate()
+    
+
+
+
+
+
--- wta.py
+++ wta.py
@@ -1,13 +1,35 @@
 
 
 
+from antcolony.py import *
+from simulatedannealing.py import *
 
 
 
 
 
+def open():
+pass
+#TODO
+
+def presentResult():
+pass
+#TODO
 
 
 
+
+print('\nHello!')
+
+data  = open()
+
+
+result = antcolonyMetaheuristic(data)
+presentResult(result)
+
+result = SM_Metaheuristic(data)
+presentResult(result)
+
+print('Bye, bye.\n')
 	
 
